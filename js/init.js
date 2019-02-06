daaf7a7d0801c448607a3d6eadf3289ef3e2456e
danfred360 <dannyfrederick360@gmail.com>
2019-02-05 22:25:19 -0500

    worked on content, navigation

--- js/init.js
+++ js/init.js
@@ -4,5 +4,6 @@
     $('.sidenav').sidenav();
     $('.parallax').parallax();
     $('.collapsible').collapsible();
+    $('.fixed-action-btn').floatingActionButton();
   }); // end of document ready
 })(jQuery); // end of jQuery name space
