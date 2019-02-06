5a832a595917bd914397304e114d1a5408eff328
danfred360 <dannyfrederick360@gmail.com>
2019-02-06 13:58:07 -0500

    started events js

---
+++ js/events.js
@@ -0,0 +1,22 @@
+class Event {
+    constructor(name, description, picture) {
+        this.name = name;
+        this.description = description;
+        this.picture = picture;
+    }
+}
+
+events = {
+    "earray" : [
+        {
+            "title" : "Google Speaker",
+            "description" : "google description",
+            "picture" : null
+        },
+        {
+            "title" : "Video Game Tounrament",
+            "description" : "game description",
+            "picture" : null
+        }
+    ]
+}
