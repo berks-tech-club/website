dc4d086ca2c653a0617dcf558477804a979cb13b
Yon Montoto <yon.montoto@gmail.com>
2019-02-27 13:56:23 -0500

    Added auto scroll to navbar. Fixed Nav to top

--- js/init.js
+++ js/init.js
@@ -8,3 +8,16 @@
     $('.slider').slider();
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+     anchor.addEventListener('click', function (e) {
+         e.preventDefault();
+
+         var el = document.querySelector(this.getAttribute('href'));
+         // .scrollIntoView({
+         //     behavior: 'smooth',
+         //     block: 'start'
+         // });
+         window.scroll({top: el.offsetTop - 40, behavior: 'smooth'});
+     });
+ });
